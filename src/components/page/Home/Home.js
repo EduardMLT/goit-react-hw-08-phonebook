3f6ee8fea984c5977aa16039b28096b3f475e326
EduardMLT <eddku@i.ua>
2023-10-21 00:20:03 +0300

    initial

--- src/components/page/Home/Home.js
+++ src/components/page/Home/Home.js
@@ -3,7 +3,7 @@ import { Title, Contaner } from './Home.styled';
 const Home = () => {
   return (
     <Contaner>
-      <Title>WELCOME TO PHONEBOOK</Title>
+      <Title>WELCOME to Phonebook</Title>
     </Contaner>
   );
 };
